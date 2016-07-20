ec6bbf6fb7642e464d48023b4b17fb8c91892143
alexhimmel <alex.ac@qq.com>
2016-07-20 10:34:44 +0000

    basic User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
  
-
   root                  'static_pages#home'
 
   get 'news'         => 'static_pages#news'
@@ -16,6 +15,9 @@ Rails.application.routes.draw do
   get 'about'        => 'static_pages#about'
   
   get 'signup'       => 'users#new'
+
+
+ 
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
